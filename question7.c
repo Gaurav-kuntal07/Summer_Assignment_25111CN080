1c5e9e2d8b8810249cd8eb3d0c2aa2779cfbca5e
Gaurav-kuntal07 <25111cn080@glbitm.ac.in>
2026-05-29 22:40:00 +0530

    Add files via upload

---
+++ question7.c
@@ -0,0 +1,16 @@
+// wap to find product ofdigits
+
+#include <stdio.h>
+int main() {
+    int num, product = 1, remainder;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if (num < 0) num = -num;
+    while (num > 0) {
+        remainder = num % 10; 
+        product = product * remainder;
+        num = num / 10;      
+    }
+    printf("Product of digits: %d\n", product);
+    return 0;
+}
